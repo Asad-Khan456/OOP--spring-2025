72c42485aafeec3fa6b3f03328509f574b2577f0
Asad-Khan456 <asadbinsadaf@gmail.com>
2025-04-11 21:36:18 +0500

    Update task 4.cpp

--- Lab 8/task 4.cpp
+++ Lab 8/task 4.cpp
@@ -2,64 +2,54 @@
 #include "string"
 using namespace std;
 class Currency{
-private:
     double value;
-
 public:
-    Currency(double v = 0.0) :value(v){}
-
-    double getValue()const{
+    Currency(double v = 0.0) : value(v){}
+    double getValue(){
         return value;
     }
-
-    void display()const{
-        cout << "Currency Value: $" << value << endl;
-    }
-
-
-
     Currency operator+(const Currency& other)const{
         return Currency(value + other.value);
     }
-    Currency operator-()const{
+    Currency operator-() const{
         return Currency(-value);
     }
-    Currency operator-(const Currency& other)const{
+
+
+    Currency operator-(const Currency& other) const{
         return Currency(value - other.value);
     }
     Currency& operator+=(const Currency& other){
         value += other.value;
         return *this;
     }
-
     Currency& operator-=(const Currency& other){
         value -= other.value;
         return *this;
     }
+    friend ostream& operator<<(ostream& os, const Currency& c){
+        os << "$" << c.value;
+        return os;
+    }
 };
-
-int main() {
-    Currency c1(102.50);
-    Currency c2(51.25);
-    cout << "c1: ";
-    c1.display();
-    cout << "c2: ";
-    c2.display();
+int main(){
+    Currency c1(100.50);
+    Currency c2(50.25);
+    cout << "c1: " << c1 << endl;
+    cout << "c2: " << c2 << endl;
     cout << endl;
     Currency c3 = c1 + c2;
-    cout << "c1 + c2 = ";
-    c3.display();
+    cout << "c1 + c2 = " << c3 << endl;
     Currency c4 = -c1;
-    cout << "-c1 = ";
-    c4.display();
+    cout << "-c1 = " << c4 << endl;
     Currency c5 = c1 - c2;
-    cout << "c1 - c2 = ";
-    c5.display();
+    cout << "c1 - c2 = " << c5 << endl;
     c1 += c2;
-    cout << "After c1 += c2, c1: ";
-    c1.display();
+    cout << "After c1 += c2, c1: " << c1 << endl;
     c1 -= c2;
-    cout << "After c1 -= c2, c1: ";
-    c1.display();
+    cout << "After c1 -= c2, c1: " << c1 << endl;
+    Currency discount(10); 
+    c1 -= discount;
+    cout << "After applying a discount of $10, c1: " << c1 << endl;
     return 0;
 }
