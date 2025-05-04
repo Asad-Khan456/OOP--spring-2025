a159f085f05126bb2ee483e0c657fbb8348bcf57
Asad-Khan456 <asadbinsadaf@gmail.com>
2025-05-04 22:30:44 +0500

    Create task 4.cpp

---
+++ Lab 10/task 4.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+using namespace std;
+int main() {
+    ifstream inFile("vehicles.txt");
+    if (!inFile.is_open()) {
+        cerr << "Error: Could not open the file 'vehicles.txt'." << endl;
+        return 1;
+    }
+    string line;
+    while (getline(inFile, line)) {
+        if(line.empty() || line[0] == '#') continue;
+        istringstream ss(line);
+        string type, id, name, year, extraData, certification;
+        getline(ss, type, ',');
+        getline(ss, id, ',');
+        getline(ss, name, ',');
+        getline(ss, year, ',');
+        getline(ss, extraData, ',');
+        getline(ss, certification, ',');
+        cout << "Type: " << type << endl;
+        cout << "ID: " << id << endl;
+        cout << "Name: " << name << endl;
+        cout << "Year: " << year << endl;
+        cout << "ExtraData: " << extraData << endl;
+        cout << "CErtification: " << certification << endl;
+        size_t colonPos = extraData.find(':');
+        if(colonPos == string::npos){
+            cout << "Warning: Invalid ExtraData format." << endl;
+        }
+        else{
+            string valueStr = extraData.substr(colonPos + 1);
+
+            if(type == "AutonomousCar")
+            {
+                float softwareVersion = stof(valueStr);
+                cout << "Software Version: " << softwareVersion << endl;
+            }
+            else if(type == "ElectricVehicle"){
+                int batteryCapacity = stoi(valueStr);
+                cout << "Battery Capacity: " << batteryCapacity << endl;
+            }
+        }
+        cout << "------------------------" << endl;
+    }
+    inFile.close();
+    return 0;
+}
