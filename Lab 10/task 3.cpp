490676fab2579df97f4f30651727561bc5b2cda8
Asad-Khan456 <asadbinsadaf@gmail.com>
2025-05-04 22:22:07 +0500

    Create task 3.cpp

---
+++ Lab 10/task 3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+using namespace std;
+int main(){
+    ifstream inFile("vehicles.txt");
+
+    if (!inFile.is_open()){
+        cerr << "Error: Could not open the file 'vehicles.txt'." << endl;
+        return 1;
+    }
+    string line;
+    while(getline(inFile, line)){
+        if (line.empty() || line[0] == '#') continue;
+        istringstream ss(line);
+        string type, id, name, yearStr, extraData, certification;
+        getline(ss, type, ',');
+        getline(ss, id, ',');
+        getline(ss, name, ',');
+        getline(ss, yearStr, ',');
+        getline(ss, extraData, ',');
+        getline(ss, certification, ',');
+        int year = stoi(yearStr);
+        cout << "Type: " << type << endl;
+        cout << "ID: " << id << endl;
+        cout << "Name: " << name << endl;
+        cout << "Year: " << year << endl;
+        cout << "ExtraData: " << extraData << endl;
+        cout << "CErtification: " << certification << endl;
+        cout << "------------------------" << endl;
+    }
+    inFile.close();
+    return 0;
+}
